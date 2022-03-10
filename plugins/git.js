3cb1ff9fb5ef70e082575abb83dd9946a9fb67d9
I4M BetA Z <94156760+I4MBetAZ@users.noreply.github.com>
2022-03-10 13:25:51 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -2,7 +2,7 @@ let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
     let devil = `ㅤ
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/iambabu98/uu/main/Hnet.com-image%20(3).jpg")).buffer(), devil, 'ᴾᴮᴬ ᴮᵒᵀ ᴮʸ ᴾᴮᴬ ᴹᵒᴰˢ 𝙼𝚁_𝚣\n\╔ₕₐₚₚy\n\║ₜₒ\n\║ₕₑₗₚ\n\╚Yₒᵤ \n\n\ https://github.com/IAMBetAZ\n\n\ ❍︎ ᵢₜz ₘ₃ ₘᵣ_z' , '❤🔥', '.❤🔥', '😗', ',😗')
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/iambabu98/uu/main/Hnet.com-image%20(3).jpg")).buffer(), devil, 'ᴾᴮᴬ ᴮᵒᵀ ᴮʸ ᴾᴮᴬ ᴹᵒᴰˢ 𝙼𝚁_𝚣\n\╔ₕₐₚₚy\n\║ₜₒ\n\║ₕₑₗₚ\n\╚Yₒᵤ \n\n\ https://github.com/I4MBetAZ/PBA-BoT-V1\n\n\ ❍︎ ᵢₜz ₘ₃ ₘᵣ_z' , '❤🔥', '.❤🔥', '😗', ',😗')
 }
 handler.tags = ['main']
 handler.help = ['git']
